0b0525116cce79dd6c82c87e1a9bbf35ace20b15
Garret Wilson <garret@globalmentor.com>
2012-01-07 23:53:48 +0000

    Improved profiler to allow packages to be ignored.

--- trunk/globalmentor-core/src/main/java/com/globalmentor/net/http/webdav/MicrosoftWebDAV.java
+++ trunk/globalmentor-core/src/main/java/com/globalmentor/net/http/webdav/MicrosoftWebDAV.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2008 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ * Copyright © 2008-2012 GlobalMentor, Inc. <http://www.globalmentor.com/>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,21 +18,23 @@ package com.globalmentor.net.http.webdav;
 
 import java.net.URI;
 
-/**WebDAV constants used by Microsoft to mount drives on WebDAV in Windows Vista.
-@author Garret Wilson
-*/
+/**
+ * WebDAV constants used by Microsoft to mount drives on WebDAV in Windows Vista.
+ * @author Garret Wilson
+ * @see <a href="http://msdn.microsoft.com/en-us/library/cc250176.aspx">WebDAV Microsoft Extensions Server Details: Server Properties</a>
+ */
 public class MicrosoftWebDAV
 {
 
-	/**The URI to the Microsoft property namespace.*/
-	public final static URI MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI=URI.create("urn:schemas-microsoft-com:");
+	/** The URI to the Microsoft property namespace. */
+	public final static URI MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI = URI.create("urn:schemas-microsoft-com:");
 
-	/**The creation timestamp in the format specified by RFC 1123.*/
-	public final static WebDAVPropertyName WIN32_CREATION_TIME=new WebDAVPropertyName(MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI, "Win32CreationTime");
-	/**The last accessed timestamp in the format specified by RFC 1123.*/
-	public final static WebDAVPropertyName WIN32_LAST_ACCESS_TIME=new WebDAVPropertyName(MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI, "Win32LastAccessTime");
-	/**The last modified timestamp in the format specified by RFC 1123.*/
-	public final static WebDAVPropertyName WIN32_LAST_MODIFIED_TIME=new WebDAVPropertyName(MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI, "Win32LastModifiedTime");
-	public final static WebDAVPropertyName WIN32_FILE_ATTRIBUTES=new WebDAVPropertyName(MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI, "Win32FileAttributes");
+	/** The creation timestamp in the format specified by RFC 1123. */
+	public final static WebDAVPropertyName WIN32_CREATION_TIME = new WebDAVPropertyName(MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI, "Win32CreationTime");
+	/** The last accessed timestamp in the format specified by RFC 1123. */
+	public final static WebDAVPropertyName WIN32_LAST_ACCESS_TIME = new WebDAVPropertyName(MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI, "Win32LastAccessTime");
+	/** The last modified timestamp in the format specified by RFC 1123. */
+	public final static WebDAVPropertyName WIN32_LAST_MODIFIED_TIME = new WebDAVPropertyName(MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI, "Win32LastModifiedTime");
+	public final static WebDAVPropertyName WIN32_FILE_ATTRIBUTES = new WebDAVPropertyName(MICROSOFT_WEBDAV_PROPERTY_NAMESPACE_URI, "Win32FileAttributes");
 
 }
